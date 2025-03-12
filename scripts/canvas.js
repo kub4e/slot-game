907fb8d4e6366ca6fe555c461d0c4f167040ca28
kub4e <tr4shm41l222@protonmail.com>
2025-03-13 01:37:06 +0200

    Optimized and documented

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,131 +1,203 @@
+const CONFIG = {
+    CANVAS_WIDTH:1200,
+    CANVAS_HEIGHT: 800,
+    TILE_SIZE: 150,
+    SPRITE_OFFSETS: {
+        cherry: 0,
+        tnt: 150,
+        coin: 300,
+    }
+}
+
 const tiles = new Image();
 tiles.src = "./assets/tiles.png"
 
-var CANVAS_WIDTH = 1200;
-var CANVAS_HEIGHT = 800;
-var tileSz = 150;
-
 var canvas = document.querySelector('canvas');
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
+canvas.width = CONFIG.CANVAS_WIDTH;
+canvas.height = CONFIG.CANVAS_HEIGHT;
 const ctx1 = canvas.getContext("2d");
 
+/**
+ * Responsible for managing a collection of reels
+ * @class
+ */
 class Game {
+    /**
+     * Stores objects of type Reel
+     */
     reels = [];
+    
+    /**
+     * Represents the entire game area
+     * @constructor
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} wdt - the width of the game area
+     * @param {number} hgh - the height of the game area
+     */
     constructor(x, y, wdt, hgh) {
         this.x = x;
         this.y = y;
         this.wdt = wdt;
         this.hgh = hgh;
+        // The number of reels depends on the size of the game space.
+        this.numReels = Math.floor(this.wdt/CONFIG.TILE_SIZE);
     }
+
+    /**
+     * Called once during initial game setup to populate the array
+     */
     genReels() {
-        let num = this.calcScreen(tileSz);
+        let num = this.numReels;
         for (let i = 0; i < num; i++) {
             let reel = new Reel(ctx1, this.y, this.hgh);
             this.reels.push(reel);
         }
     }
+
+    /**
+     * Called once during initial game setup to draw the initial game state
+     * (Could probably be merged with genReels)
+     * @param {CanvasRenderingContext2D} ctx - the context for canvas operations
+     */
     drawGame(ctx) {
-        let num = this.calcScreen(tileSz); // TODO UPDATE HOW TILE SIZE IS PASSED
-        for (let i = 0, tx = 0; i < num; i++, tx+=tileSz) {
+        let num = this.numReels;
+        for (let i = 0, tx = 0; i < num; i++, tx+=CONFIG.TILE_SIZE) {
             this.reels[i].drawReel(tx, this.y, ctx);
         }
     }
-    calcScreen(tSz) {
-        let numReels = Math.floor(this.wdt/tSz);
-        return numReels;
-    }
 }
 
+/**
+ * Responsible for managing tiles individually
+ * @class
+ */
 class Tile {
     isDisplayed = false;
-    isWin = false;
-    isOld = false;
-    #tSz = tileSz;
-    #types = {
-        "cherry": [0, 0],
-        "tnt": [150, 0],
-        "coin": [300, 0]
-    }
+    #tSz = CONFIG.TILE_SIZE;
+
+    /**
+     * Represents a single tile
+     * @constructor
+     * @param {CanvasRenderingContext2D} ctx - the context for canvas operations
+     * @param {number} gTop - The top limit of the game (px)
+     * @param {number} gBot - The bottom limit of the game (px)
+     */
     constructor(ctx, gTop, gBot) {
         this.gTop = gTop;
         this.gBot = gBot;
         this.ctx = ctx;
-        let keys = Object.keys(this.#types);
-        this.type = keys[keys.length * Math.random() << 0];
+        let types = Object.keys(CONFIG.SPRITE_OFFSETS);
+        this.type = types[types.length * Math.random() << 0];
     }
+
+    /**
+     * Tiles are drawn with dynamic sizing based on vertical position.
+     * Handles rendering and state.
+     * @param {number} x - Horizontal position (px)
+     * @param {number} y - Vertical position (px)
+     */
     drawTile(x, y) {
         this.x = x;
         this.y = y;
-        if (y + this.#tSz >= this.gBot) {
-            if (this.#tSz <= 0) {
-                this.#tSz = 0;
-            }
-            else {
-                this.#tSz -= 1;
-            }
-        }
-        else {
-            this.#tSz = tileSz;
-        }
-        this.ctx.drawImage(tiles, this.#types[this.type][0], this.#types[this.type][1], 150, this.#tSz, x, y, 150, this.#tSz);
         
+        this.ctx.drawImage(
+            tiles,                              // Image
+            CONFIG.SPRITE_OFFSETS[this.type],   // Source x from sprite sheet
+            0,                                  // Source y (for now only one row of tiles)
+            CONFIG.TILE_SIZE,                   // Source width
+            CONFIG.TILE_SIZE,                   // Source height
+            x,                                  // Destination x
+            y,                                  // Destination y
+            CONFIG.TILE_SIZE,                   // Render width
+            CONFIG.TILE_SIZE                    // Render height
+        );
         
-        if (this.y <= this.gBot) {
-            this.isDisplayed = true;
-        } else {
-            this.isDisplayed = false;
-            //this.clear();
-
-        }
+        // Used by Reel.spin() to recycle offscreen tiles
+        this.isDisplayed = this.y + CONFIG.TILE_SIZE > this.gTop &&
+                            this.y < this.gBot;
     }
+
+    /**
+     * Movement is based on framerate (needs to be updated)
+     * @param {number} speed - Pixels per frame
+     * @param {CanvasRenderingContext2D} ctx - the context for canvas operations
+     */
     mvDown(speed, ctx) {
-        ctx.clearRect(this.x, this.y, tileSz, tileSz);
+        ctx.clearRect(this.x, this.y, CONFIG.TILE_SIZE, CONFIG.TILE_SIZE);
         this.y += speed;
         this.drawTile(this.x, this.y);     
     }
     clear() {
-        this.ctx.clearRect(this.x, this.y, 150, 150)
+        this.ctx.clearRect(this.x, this.y, CONFIG.TILE_SIZE, CONFIG.TILE_SIZE)
     }
 }
 
+/**
+ * Responsible for managing tiles within a single reel
+ * @class
+ */
 class Reel {
     elements = []
+    /**
+     * Represents a single reel
+     * @constructor
+     * @param {CanvasRenderingContext2D} ctx - the context for canvas operations
+     * @param {number} gTop - The top limit of the game (px)
+     * @param {number} gBot - The bottom limit of the game (px)
+     */
     constructor(ctx, gTop, gBot) {
         this.gTop = gTop;
         this.gBot = gBot;
         this.ctx = ctx;
-        //this.eNum = eNum;
-        this.eNum = this.calcElements(tileSz, this.gTop, this.gBot);
-        this.elements.length = this.eNum;
-        for (let i = 0; i < this.eNum; i++) {
+        this.tileCount = Math.floor((gBot - gTop) / CONFIG.TILE_SIZE);
+        this.elements.length = this.tileCount;
+
+        for (let i = 0; i < this.tileCount; i++) {
             this.elements[i] = new Tile(this.ctx, gTop, gBot);
         }
     }
-    calcElements(tSz, gTop, gBot) {
-        return Math.floor((gBot - gTop) / tSz);
-    }
+
+    /**
+     * Represents a single reel
+     * @param {CanvasRenderingContext2D} ctx - the context for canvas operations
+     * @param {number} gTop - The top limit of the game (px)
+     * @param {number} gBot - The bottom limit of the game (px)
+     */
     drawReel(x, y, ctx) {
         this.x = x;
         this.y = y;
-        for (let i = 0; i < this.eNum; i++, y+=150) {
-            //this.elements[(this.eNum-1)-i].drawTile(x, y, ctx);
+        for (let i = 0; i < this.tileCount; i++, y+=150) {
             this.elements[i].drawTile(x, y, ctx);
         }
     }
+
+    /**
+     * Clearing is handled per reel to allow for reels to move independent from one another
+     */
     clear() {
-        for (let i = 0; i < this.eNum; i++) {
+        for (let i = 0; i < this.tileCount; i++) {
             this.elements[i].clear();
         }
     }
+
+    /**
+     * Only handles logic relies on tile.mvDown() for rendering
+     * @param {number} speed - pixels per frame
+     * @param {CanvasRenderingContext2D} ctx - the context for canvas operations
+     */
     spin(speed, ctx) {
-        for (let i = 0; i < this.eNum; i++) {
-            this.elements[i].mvDown(speed, ctx);
-            if (this.elements[i].isDisplayed == false) {
-                let tmp = this.elements.pop();
-                tmp.y = this.elements[0].y - tileSz;
-                this.elements.unshift(tmp);
-            }
+        let needsRecycle = false;
+
+        for (const tile of this.elements) {
+            tile.mvDown(speed, ctx);
+            if (!tile.isDisplayed) needsRecycle = true;
+        }
+
+        if (needsRecycle) {
+            const lastTile = this.elements.pop();
+            lastTile.y = this.elements[0].y - CONFIG.TILE_SIZE;
+            this.elements.unshift(lastTile);
         }
     }
 }
@@ -139,7 +211,7 @@ tiles.onload = function () {
 
 async function spinReel() {
     for (let i = 0; i < game.reels.length; i++) {
-        game.reels[0].spin(10, ctx1);
+        game.reels[i].spin(10, ctx1);
     }
 
     requestAnimationFrame(spinReel);
